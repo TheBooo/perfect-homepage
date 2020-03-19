0e5777e91050ef274ce2d6effd7515b4535ee8a8
TheBoo19 <fearntn@yandex.ru>
2020-03-19 17:57:19 +0300

    logotype, search, uuid+icons

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchForm from "./components/SearchForm";
+import Weather from "./components/Weather";
+import Logo from "./components/Logo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logo />
+      <SearchForm />
+      <Weather />
+    </>
   );
 }
 
---
+++ app/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../img/logo.png";
+
+const Logo = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <img className="logotype" src={logo} />
+      </div>
+    </div>
+  );
+};
+export default Logo;
