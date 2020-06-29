894d01fd1e45129e4c871344a822d28008a61d99
Ivan Egorov <fearntn@yandex.ru>
2020-06-29 12:17:45 +0300

    added production build

---
+++ precache-manifest.c508d78ebdebfffb493eb7865809aa31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2226eef0918ff45b17e608267cc5cfdd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97c4933bcbed1c1adf6c",
+    "url": "./static/css/main.74a4d23f.chunk.css"
+  },
+  {
+    "revision": "e86b8c65982d468f0fdb",
+    "url": "./static/js/2.51cf5be2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.51cf5be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c4933bcbed1c1adf6c",
+    "url": "./static/js/main.cd6b4612.chunk.js"
+  },
+  {
+    "revision": "628eaaaaf82a98dd9226",
+    "url": "./static/js/runtime-main.a708e874.js"
+  },
+  {
+    "revision": "02ce1dec353ca0d5f52f67c620e43533",
+    "url": "./static/media/Duckduckgo.02ce1dec.svg"
+  },
+  {
+    "revision": "f409b8705d27b8c574ae3fa0340a7804",
+    "url": "./static/media/logo.f409b870.png"
+  }
+]);
