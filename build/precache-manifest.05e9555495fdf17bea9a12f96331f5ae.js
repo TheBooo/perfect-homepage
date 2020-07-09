08bfb0693404715f193dd5eaf3634d649f7da746
Ivan Egorov <41519993+TheBooo@users.noreply.github.com>
2020-07-09 12:48:43 +0300

    Add files via upload

---
+++ build/precache-manifest.05e9555495fdf17bea9a12f96331f5ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5a3fc2503bc836ee426e5c73eb8a6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27d21132009125a1f8e5",
+    "url": "./static/css/main.74a4d23f.chunk.css"
+  },
+  {
+    "revision": "e86b8c65982d468f0fdb",
+    "url": "./static/js/2.51cf5be2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.51cf5be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d21132009125a1f8e5",
+    "url": "./static/js/main.6a55f237.chunk.js"
+  },
+  {
+    "revision": "628eaaaaf82a98dd9226",
+    "url": "./static/js/runtime-main.a708e874.js"
+  },
+  {
+    "revision": "02ce1dec353ca0d5f52f67c620e43533",
+    "url": "./static/media/Duckduckgo.02ce1dec.svg"
+  },
+  {
+    "revision": "f409b8705d27b8c574ae3fa0340a7804",
+    "url": "./static/media/logo.f409b870.png"
+  }
+]);
